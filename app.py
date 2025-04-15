0591b642eeb440dbb1c668e8cdff3acd75d2c12b
Aayush-lad <aayushlad1973@gmail.com>
2025-04-15 22:56:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -177,5 +177,7 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get('PORT', 5000))  # fallback for local dev
+    app.run(host='0.0.0.0', port=port)
